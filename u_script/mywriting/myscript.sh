43bc71f70b05fabdacde4a4377a62d8fd75ed883
yoowonsuk <seo0093@naver.com>
2023-12-16 11:08:17 +0900

    path in .file

---
+++ u_script/mywriting/myscript.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+echo hello this is my script
+
+#this is a comment for myself
+#they do not affter in any way the program
+
+# TextMate
+# Sublime Text
+# Atom
+# MacVim
+# Brackets
+
+echo " $((2+3)) "
