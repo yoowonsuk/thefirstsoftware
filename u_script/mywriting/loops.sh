d96f170b06d5b98ee3be24f3763f3cef53f708b3
yoowonsuk <seo0093@naver.com>
2023-12-16 14:42:12 +0900

    case

--- u_script/mywriting/loops.sh
+++ u_script/mywriting/loops.sh
@@ -15,3 +15,36 @@ for i in ./*
 do
 	echo "name of file is: $i"
 done
+
+' $a '  << only string
+" $a " << accept variables
+
+number=1
+while [ $number -le 7 ];
+do
+	echo "number is $number"
+	number=$((number+4))
+done
+
+sleep 0.5
+ctrl + c or ctrl + z
+
+read choice
+case $choice in
+	1)
+		echo "You Choose A"
+		;;
+	2)
+		echo "You Choose B"
+		;;
+	*)      echo "None of The Above"
+	"Hello")
+	*".txt")
+	[0-9])
+	[a-z])
+	[A-Z])
+	1[2-8])
+	[1-3] | [7-9])
+esca
+
+
