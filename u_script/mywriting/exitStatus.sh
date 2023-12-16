95d7ce491d147077b2b998298ef6753cd82c9786
yoowonsuk <seo0093@naver.com>
2023-12-16 14:44:55 +0900

    exitStatus

---
+++ u_script/mywriting/exitStatus.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+echo $?
+=> 0
+
+pwd - asdfsdf
+echo $?
+=> 1
+
+
+pasdfs
+echo $?
+=> 127
+
+# man pwd
+# man grep
+
+grep "end" $file
+echo $?
+=> 0 or 1
+
+file not there
+=> 2
+
